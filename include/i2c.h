5df4f5b61ab0b313061ffba422c932e231216371
Ryan Vo <rv479@drexel.edu>
2026-07-03 02:48:57 -0400

    Modify I2C_ReadRegister() parameter

--- include/i2c.h
+++ include/i2c.h
@@ -7,6 +7,6 @@ typedef enum {
 
 void I2C_Init(void);
 I2C0_STATUS I2C_WriteRegister(uint8_t slaveAddr, uint8_t regAddr, uint8_t data);
-I2C0_STATUS I2C_ReadRegister(uint8_t slaveAddr, uint8_t regAddr, uint8_t data);
+I2C0_STATUS I2C_ReadRegister(uint8_t slaveAddr, uint8_t regAddr, uint8_t *data);
 
 #endif
